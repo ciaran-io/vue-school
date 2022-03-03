3a127c271c6151ea7de5075d010eb10e5e8e4277
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-03-03 21:34:33 +0000

    feat: initial commit with setup

---
+++ Pinia Tutorial/src/stores/ProductStore.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia'
+import products from '@/data/products.json'
+export const useProductStore = defineStore('ProductStore', {
+  state: () => ({
+
+    products
+
+  })
+})
