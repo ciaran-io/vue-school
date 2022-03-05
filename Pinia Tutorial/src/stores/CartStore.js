8e96af967660532f821bf72ffc7dd28e8964e648
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-03-05 15:26:11 +0000

    refactor: sort grouped cart items
    1. Grouped function now sorts cart items by name.
    2. function to remove all cart items.
    3. function to set cart item ammount values.

--- Pinia Tutorial/src/stores/CartStore.js
+++ Pinia Tutorial/src/stores/CartStore.js
@@ -3,7 +3,6 @@ import { groupBy } from 'lodash'
 export const useCartStore = defineStore('cartStore', {
   state: () => ({
     items: [],
-
   }),
 
   // computed state
@@ -13,19 +12,42 @@ export const useCartStore = defineStore('cartStore', {
     // return cart count if length is equal to 0
     isEmpty: (state) => state.count === 0,
     // return grouped products that are the same product
-    grouped: (state) => groupBy(state.items, (item) => item.name)
+    grouped: (state) => {
+      const grouped = groupBy(state.items, (item) => item.name)
+      // soryt grouped order
+      const sorted = Object.keys(grouped).sort()
+      const inOrder = {}
+      // add stored group key value to inOrder object
+      sorted.forEach(key => inOrder[key] = grouped[key])
+      return inOrder
 
+    },
+
+    groupCount: (state) => (name) => state.grouped[name].length,
+
+    cartTotal: (state) =>
+      state.items.reduce((prevValue, currentValue) =>
+        prevValue + currentValue.price, 0)
 
   },
   // mautate state
   actions: {
-    addItems(count, item) {
+    addCartItem(count, item) {
       count = parseInt(count)
       for (let i = 0; i < count; i++) {
         // spread operator clones item
         // passes object by value not by reference
         this.items.push({ ...item })
       }
+    },
+
+    removeCartItem(itemName) {
+      this.items = this.items.filter(item => item.name !== itemName)
+    },
+
+    setItemCount(item, count) {
+      this.removeCartItem(item.name)
+      this.addCartItem(count, item)
     }
   }
 
