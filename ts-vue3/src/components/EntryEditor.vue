a7472812e335f890622127ba8cd49660f4fea7d2
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-12-09 10:01:15 +0000

    feat: add limit to textarea
    1. Add limit to textarea with types to event interface.
    2. Define max character count and add to DOM.

--- ts-vue3/src/components/EntryEditor.vue
+++ ts-vue3/src/components/EntryEditor.vue
@@ -7,16 +7,29 @@ import { ref, computed } from "vue";
 const text = ref("");
 const emoji = ref<Emoji | null>(null);
 const charCount = computed(() => text.value.length);
+const maxCharCount = 280;
+// Remove text inout after 280 characters
+const handleTextInput = (event: Event) => {
+  const textarea = event.target as HTMLInputElement;
+
+  if (textarea.value.length <= maxCharCount) {
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxCharCount);
+  }
+};
 </script>
+
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea
-      v-model="text"
+      @keyup="handleTextInput"
+      :value="text"
       placeholder="New Journal Entry for danielkelly_io"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>{{ charCount }} / 280</span>
+      <span>{{ charCount }} / {{ maxCharCount }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
