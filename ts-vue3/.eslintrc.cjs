033e81ce03d574837c1173567d0b5e47b04cede5
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2022-12-09 12:06:20 +0000

    chore: update eslint config
    1. Add stricter vue linting rules.
    2. Ensure single line attributes in templates.

--- ts-vue3/.eslintrc.cjs
+++ ts-vue3/.eslintrc.cjs
@@ -4,12 +4,20 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "plugin:vue/vue3-recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
   env: {
     "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleAttributePerLine: true,
+      },
+    ],
   },
 };
