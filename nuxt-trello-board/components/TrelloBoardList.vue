daef4f0fb821422d0acaf680f725ef6f2c45a740
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-02-07 13:42:20 +0000

    feat: add trello board list component

---
+++ nuxt-trello-board/components/TrelloBoardList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import type { Task } from '~/types'
+
+  defineProps<{ task: Task }>()
+</script>
+
+<template>
+  <div
+    class="rounded bg-white p-2 shadow-sm"
+    :title="task.createdAt.toLocaleDateString()"
+  >
+    <span>{{ task.title }}</span>
+  </div>
+</template>
