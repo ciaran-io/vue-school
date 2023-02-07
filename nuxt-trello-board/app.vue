cdb83bcc7240d6bff633e089b1bc22095deb8b2e
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-02-07 13:43:52 +0000

    feat: add heading, update styles

--- nuxt-trello-board/app.vue
+++ nuxt-trello-board/app.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-  <div class='container h-[100vh] bg-cyan-600 overflow-auto pt-4'>
+  <div class='h-[100vh] bg-cyan-600 overflow-auto pt-4 space-y-4'>
+    <h1 class='text-xl text-white container'>Trello Board</h1>
    <TrelloBoard />
   </div>
 </template>
